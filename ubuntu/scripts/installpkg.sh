3889c78d2305dc4fa410a530d3b7a631f5353c45
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-13 00:52:23 +0700

    [Ubuntu] Add some more lib

--- ubuntu/scripts/installpkg.sh
+++ ubuntu/scripts/installpkg.sh
@@ -25,7 +25,7 @@ sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
     libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
     gfortran openexr libatlas-base-dev python3-dev python3-numpy \
-    libtbb2 libtbb-dev libdc1394-22-dev
+    libtbb2 libtbb-dev libdc1394-22-dev libavresample-dev
 
 # * OpenGL
 sudo apt install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev
