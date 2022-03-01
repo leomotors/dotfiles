32f589a60efd7bd434a9fd4be4afec34d2ac76fe
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-03-02 00:34:15 +0700

    Add .zprofile

---
+++ ubuntu/.zprofile
@@ -0,0 +1,7 @@
+export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar";
+export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew";
+export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin";
+export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}";
+
--- ubuntu/scripts/linkdot.sh
+++ ubuntu/scripts/linkdot.sh
@@ -1,19 +1,22 @@
 # * Link Dotfiles
 
-rm ~/.zshrc
+rm -f ~/.zshrc
 ln -s ~/dotfiles/ubuntu/.zshrc ~/.zshrc
 
-rm ~/.p10k.zsh
+rm -f ~/.p10k.zsh
 ln -s ~/dotfiles/ubuntu/.p10k.zsh ~/.p10k.zsh
 
-rm ~/.bashrc
+rm -f ~/.bashrc
 ln -s ~/dotfiles/ubuntu/.bashrc ~/.bashrc
 
-rm ~/.bash_logout
+rm -f ~/.bash_logout
 ln -s ~/dotfiles/ubuntu/.bash_logout ~/.bash_logout
 
-rm ~/.profile
+rm -f ~/.profile
 ln -s ~/dotfiles/ubuntu/.profile ~/.profile
 
-rm ~/.gitconfig
+rm -f ~/.zprofile
+ln -s ~/dotfiles/ubuntu/.zprofile ~/.zprofile
+
+rm -f ~/.gitconfig
 ln -s ~/dotfiles/ubuntu/.gitconfig ~/.gitconfig
