da5fe91da28578ba97aa5a6599609e25a6b41b49
Nutthapat Pongtanyavichai <contact@leomotors.me>
2024-09-29 01:18:23 +0700

    feat: emurock5

---
+++ emurock5/link.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+rm -f ~/.gitconfig
+ln -s ~/dotfiles/emurock5/.gitconfig ~/.gitconfig
+
+rm -f ~/.zshrc
+ln -s ~/dotfiles/emurock5/.zshrc ~/.zshrc
--- honamipi5/link.zsh
+++ honamipi5/link.zsh
@@ -3,8 +3,5 @@
 rm -f ~/.gitconfig
 ln -s ~/dotfiles/honamipi5/.gitconfig ~/.gitconfig
 
-rm -f ~/.zprofile
-ln -s ~/dotfiles/honamipi5/.zprofile ~/.zprofile
-
 rm -f ~/.zshrc
 ln -s ~/dotfiles/honamipi5/.zshrc ~/.zshrc
